100d7ceefff00ba82e0e00c00079ed9f8a88b31d
Fleur Jarman <fleur.jarman@granicus.com>
2020-08-31 14:23:12 +0100

    open debuggin

--- javascript/sendMail.js
+++ javascript/sendMail.js
@@ -1,17 +1,20 @@
 function sendMail(contactForm) {
-   email.js.send("gmail", "yummy", {
+   emailjs.send("gmail", "yummy", {
       from_name: contactForm.name.value,
       from_email: contactForm.emailaddress.value,
-      recipe_name: contactForm.recipe_name.value, 
+      recipe_name: contactForm.recipe_name.value,
+      ingredients: contactForm.ingredients.value,
       method: contactForm.method.value
+
    })
-   then(
-      function (response) {
-         console.log("SUCCESS", response);
-      },
-      function (error) {
-         console.log("FAILED", error);
-      });
+      .then(
+         function (response) {
+            console.log("SUCCESS", response);
+         },
+         function (error) {
+            console.log("FAILED", error);
+         }
+      );
 }
 
 
